4f259274f362ac16b1d11f838e653ef9e30dc826
hogaur <hogaur@gmail.com>
2023-01-12 18:25:58 +0530

    Adds standard i/o

--- main.go
+++ main.go
@@ -3,5 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello World")
+	conferenceName := "Go Conference"
+	conferenceTickets := 50
+	remainingTickets := 50
+
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v tickets are still available.\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here")
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets int
+
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Println("Enter your email: ")
+	fmt.Scan(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	remainingTickets = remainingTickets - userTickets
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v, \n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v \n", remainingTickets, conferenceName)
 }
