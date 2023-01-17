6e1e00050efcffc3845394752ad318b984803108
hogaur <hogaur@gmail.com>
2023-01-18 02:56:41 +0530

    Adds user input validation

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	conferenceName := "Go Conference"
@@ -30,22 +33,40 @@ func main() {
 		fmt.Println("Enter number of tickets: ")
 		fmt.Scan(&userTickets)
 
-		for userTickets > remainingTickets {
-			fmt.Printf("We have a total of %v tickets and %v tickets are still available.\n", conferenceTickets, remainingTickets)
-			fmt.Println("Enter number of tickets: ")
-			fmt.Scan(&userTickets)
-		}
+		isValidName := len(firstName) >= 1 && len(lastName) >= 1
+		isValidEmail := IsValidEmail(email)
+		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
 
-		if userTickets != 0 {
+		if isValidName && isValidEmail && isValidTicketNumber {
 			bookings = append(bookings, firstName+" "+lastName)
 
 			fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v, \n", firstName, lastName, userTickets, email)
-			fmt.Printf("The whoel slice : %v\n", bookings)
-			fmt.Printf("The first value : %v\n", bookings[0])
-			fmt.Printf("The slice type : %T\n", bookings)
-			fmt.Printf("The slice length : %v\n", len(bookings))
-		}
+		} else {
+			if !isValidName {
+				fmt.Println("Invalid First name of last name. Both need to be longer than 1 character.")
+				fmt.Printf("We have a total of %v tickets and %v tickets are still available.\n", conferenceTickets, remainingTickets)
+				fmt.Println("Enter your first name: ")
+				fmt.Scan(&firstName)
 
+				fmt.Println("Enter your last name: ")
+				fmt.Scan(&lastName)
+			}
+			if !isValidEmail {
+				fmt.Println("Invalid Email. Email needs a @.")
+				fmt.Printf("We have a total of %v tickets and %v tickets are still available.\n", conferenceTickets, remainingTickets)
+				fmt.Println("Enter your email: ")
+				fmt.Scan(&email)
+				if !IsValidEmail(email) {
+					continue
+				}
+			}
+			if !isValidTicketNumber {
+				fmt.Println("Invalid Ticket Number. Please enter a valid ticket number.")
+				fmt.Printf("We have a total of %v tickets and %v tickets are still available.\n", conferenceTickets, remainingTickets)
+				fmt.Println("Enter number of tickets: ")
+				fmt.Scan(&userTickets)
+			}
+		}
 		remainingTickets = remainingTickets - userTickets
 
 		fmt.Printf("%v tickets remaining for %v \n", remainingTickets, conferenceName)
@@ -56,5 +77,9 @@ func main() {
 			break
 		}
 	}
+}
 
+func IsValidEmail(email string) bool {
+	isValidEmail := strings.Contains(email, "@")
+	return isValidEmail
 }
